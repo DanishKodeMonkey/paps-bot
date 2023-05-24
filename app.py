d8ff0aed7efc8559b337af19b9bd5ef61826f200
zanderhavgaard <zander@havgaard.dk>
2023-05-25 00:48:26 +0200

    fix sys.exit call

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+"""
+Discord bot for coordinating pen-and-paper-shenanigans
+"""
 import os
 import sys
 import discord
 
 TOKEN = os.environ.get("DISCORD_TOKEN", None)
 if not TOKEN:
-    sys.exit(1, "ERROR: Could not find env var DISCORD_TOKEN, exitting.")
+    print("ERROR: Could not find env var DISCORD_TOKEN, exitting.")
+    sys.exit(1)
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -14,11 +18,13 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
+    """Executed when the bot joins the discord server"""
     print(f"We have logged in as {client.user}")
 
 
 @client.event
 async def on_message(message):
+    """executed when a message is sent that the bot can read"""
     if message.author == client.user:
         return
 
